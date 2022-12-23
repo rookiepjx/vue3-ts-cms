eb0b7f943378c7671ee727bd902dd7d8bf1abcb1
Ronan <1254198616@qq.com>
2022-12-23 18:08:13 +0800

    feat:初始化项目

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,17 @@
-const { defineConfig } = require("@vue/cli-service");
+const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
+const resolve = (dir) => {
+  return path.resolve(__dirname, dir)
+}
 module.exports = defineConfig({
+  outputDir: './build',
   transpileDependencies: true,
-});
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  }
+})
