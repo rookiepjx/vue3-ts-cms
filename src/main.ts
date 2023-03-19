ef058e333210a1efae373fd81d253257fb224ed7
Ronan <1254198616@qq.com>
2023-03-19 17:05:54 +0800

    feat:样式/资源初始化

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引用样式
+import 'normalize.css'
+import './assets/css/index.less'
+
 // 全局注册 例如elementUi
 import { globalRegister } from '@/global'
 // 封装axios
