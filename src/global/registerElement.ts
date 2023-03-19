735451ad94f119363d96cff719db95682c202e2d
Ronan <1254198616@qq.com>
2023-03-19 17:06:54 +0800

    feat:登录页面初始化

--- src/global/registerElement.ts
+++ src/global/registerElement.ts
@@ -1,9 +1,29 @@
 import { App } from 'vue'
 // 导入公共基础css
 import 'element-plus/theme-chalk/index.css'
-import { ElButton, ElTag } from 'element-plus/lib/index'
+import {
+  ElButton,
+  ElCheckbox,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElLink,
+  ElRadio,
+  ElTabPane,
+  ElTabs
+} from 'element-plus/lib/index'
 
-const components = [ElButton, ElTag]
+const components = [
+  ElButton,
+  ElCheckbox,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElLink,
+  ElRadio,
+  ElTabPane,
+  ElTabs
+]
 
 export default function (app: App): void {
   for (const component of components) {
