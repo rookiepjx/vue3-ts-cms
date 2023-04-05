0817da1fce33b1370759ba80543ec78c62e280d5
Ronan <1254198616@qq.com>
2023-04-05 15:33:11 +0800

    feat:请求添加token

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,11 +1,17 @@
 import MyRequest from './request'
 import { BASE_URL, TIME_OUT } from './request/config'
+import localCache from '@/utils/cache'
 
 const httpRequest = new MyRequest({
   baseURL: BASE_URL,
   timeout: TIME_OUT,
   interceptors: {
     requestInterceptor: (config) => {
+      // 携带token的拦截
+      const token = localCache.getCache('token')
+      if (token && config && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`
+      }
       console.log('请求成功拦截', config)
       return config
     },
